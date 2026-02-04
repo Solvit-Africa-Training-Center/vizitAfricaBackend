97639723c3fe67716addb8736ea4ec854112d364
Don-tresor05 <dontresor922@gmail.com>
2026-02-04 08:27:10 +0200

    update to use settings.AUTH_USER_MODEL

--- transactions/models.py
+++ transactions/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.conf import settings
 
 class Transaction(models.Model):
     TRANSACTION_TYPES = [
@@ -15,7 +15,7 @@ class Transaction(models.Model):
     ]
 
     booking = models.ForeignKey('bookings.Booking', on_delete=models.CASCADE, related_name='transactions')
-    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='transactions')
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='transactions')
     amount = models.DecimalField(max_digits=12, decimal_places=2)
     currency = models.CharField(max_length=3, default='USD')
     transaction_type = models.CharField(max_length=20, choices=TRANSACTION_TYPES)
