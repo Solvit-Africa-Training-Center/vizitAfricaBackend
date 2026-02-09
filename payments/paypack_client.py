f1b1669e59c84dbfafc046b9cf403d13045293b5
Don-tresor05 <dontresor922@gmail.com>
2026-02-09 16:28:01 +0200

    removed sensitive crentials in this file

--- payments/paypack_client.py
+++ payments/paypack_client.py
@@ -3,10 +3,12 @@ from paypack.client import HttpClient
 from paypack.transactions import Transaction
 from paypack.oauth2 import Oauth
 import os
+# from dotenv import load_dotenv
 
-# Load credentials from environment variables
-CLIENT_ID = os.getenv("PAYPACK_CLIENT_ID", "7860b702-00e3-11f1-a169-deadd43720af")
-CLIENT_SECRET = os.getenv("PAYPACK_CLIENT_SECRET", "d70805d29c435df9d110e1515b02b204da39a3ee5e6b4b0d3255bfef95601890afd80709")
+# load_dotenv()  # loads .env into environment
+
+CLIENT_ID = os.getenv("PAYPACK_CLIENT_ID")
+CLIENT_SECRET = os.getenv("PAYPACK_CLIENT_SECRET")
 
 # Initialize HttpClient
 client = HttpClient(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
