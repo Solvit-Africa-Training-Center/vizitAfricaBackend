293556aa22f0bb8a22348892bc858da44c19623f
Don-tresor05 <dontresor922@gmail.com>
2026-02-09 16:30:13 +0200

    changed the import

--- payments/urls.py
+++ payments/urls.py
@@ -1,8 +1,8 @@
 # payments/urls.py
 from django.urls import path
-from .views import CashInView, CashOutView
+from .views import payload, CashOutView
 
 urlpatterns = [
-    path("cashin/", CashInView.as_view(), name="cashin"),
+    path("cashin/", payload.as_view(), name="cashin"),
     path("cashout/", CashOutView.as_view(), name="cashout"),
 ]
