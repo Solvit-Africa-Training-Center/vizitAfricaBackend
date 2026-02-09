2a8a4602fab9ac18f7c01d621c1b8d3bd3da5736
Don-tresor05 <dontresor922@gmail.com>
2026-02-09 12:54:17 +0200

    add phone_number in REQUIRED_FIELDS

--- accounts/models.py
+++ accounts/models.py
@@ -82,7 +82,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     objects = UserManager()
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["full_name"]
+    REQUIRED_FIELDS = ["full_name", "phone_number"]
 
     def __str__(self):
         return self.email
